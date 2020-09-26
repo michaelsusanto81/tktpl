705656414b68a539e7f84f3f276ef6fcaef019fe
Michael Susanto <michaelsusanto32@gmail.com>
2020-09-26 21:44:24 +0700

    add: login and register page

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/auth/login/LoginFragment.kt
@@ -1,36 +1,36 @@
-package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
+package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.ui.auth.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
-import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentFirstBinding
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.R
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-    private lateinit var binding: FragmentFirstBinding
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_RegisterFragment)
         }
     }
 }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/auth/register/RegisterFragment.kt
@@ -1,36 +1,36 @@
-package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
+package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.ui.auth.register
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
-import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentSecondBinding
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.R
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentRegisterBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
-    private lateinit var binding: FragmentSecondBinding
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
         }
     }
 }
