cad87f770ade95ed6217eabd4eb71df13bc15f00
Michael Susanto <michaelsusanto32@gmail.com>
2020-10-03 21:41:58 +0700

    add Stopwatch functionality

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/MainActivity.kt
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
 
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.ActivityMainBinding
 
@@ -13,8 +14,48 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val br: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            updateView(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.buttonStart.setOnClickListener {
+            startService(Intent(this, StopwatchService::class.java))
+        }
+
+        binding.buttonStop.setOnClickListener {
+            stopService(Intent(this, StopwatchService::class.java))
+            binding.textViewTimer.setText(R.string.stopwatch_time)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(br, IntentFilter(StopwatchService.STOPWATCH_BR))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(br)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(Intent(this, StopwatchService::class.java))
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateView(intent: Intent?) {
+        if(intent?.extras != null) {
+            val hours: String = intent.getStringExtra("hours")!!
+            val minutes: String = intent.getStringExtra("minutes")!!
+            val seconds: String = intent.getStringExtra("seconds")!!
+            binding.textViewTimer.text = "$hours:$minutes:$seconds"
+        }
     }
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/StopwatchService.kt
@@ -0,0 +1,76 @@
+package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+
+class StopwatchService: Service() {
+
+    companion object {
+        const val STOPWATCH_BR: String = "com.stopwatch.stopwatch"
+    }
+
+    private val REFRESH_RATE: Long = 100;
+    private val bi: Intent = Intent(STOPWATCH_BR)
+    private val mHandler: Handler = Handler()
+    private var stopped: Boolean = false;
+
+    private var startTime: Long = 0
+    private var elapsedTime: Long = 0
+    private var hrs: Long = 0
+    private var mins: Long = 0
+    private var secs: Long = 0
+
+    private var hours: String = ""
+    private var minutes: String = ""
+    private var seconds: String = ""
+
+    private val startTimer: Runnable = object : Runnable {
+        override fun run() {
+            elapsedTime = System.currentTimeMillis() - startTime
+            updateTimer(elapsedTime)
+            mHandler.postDelayed(this, REFRESH_RATE)
+        }
+    }
+
+    private fun updateTimer(elapsedTime: Long) {
+        secs = elapsedTime/1000
+        mins = secs/60
+        hrs = mins/60
+
+        secs %= 60
+        seconds = secs.toString().padStart(2, '0')
+        mins %= 60
+        minutes = mins.toString().padStart(2, '0')
+        hours = hrs.toString().padStart(2, '0')
+
+        bi.putExtra("hours", hours)
+        bi.putExtra("minutes", minutes)
+        bi.putExtra("seconds", seconds)
+        sendBroadcast(bi)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if(stopped) {
+            startTime = System.currentTimeMillis() - elapsedTime
+        } else {
+            startTime = System.currentTimeMillis()
+        }
+
+        mHandler.removeCallbacks(startTimer)
+        mHandler.postDelayed(startTimer, 0)
+    }
+
+    override fun onDestroy() {
+        mHandler.removeCallbacks(startTimer)
+        stopped = true
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
