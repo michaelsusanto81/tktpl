7ab9801a5b2a9e95bada56561a67ce473a636e4a
Michael Susanto <michaelsusanto32@gmail.com>
2020-10-07 01:30:56 +0700

    add data binding

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/FirstFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/FirstFragment.kt
@@ -1,30 +1,35 @@
-package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
+package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.R
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var binding: FragmentFirstBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/SecondFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/ui/SecondFragment.kt
@@ -1,31 +1,28 @@
-package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld
+package id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.R
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
+    private lateinit var binding: FragmentSecondBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        return binding.root
     }
 }
