08f7dc19ede4f5df32f14c287bde714f1b2fec32
Michael Susanto <michaelsusanto32@gmail.com>
2020-11-24 21:26:52 +0700

    add binding

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/WifiFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/michaelsusanto/helloworld/WifiFragment.kt
@@ -5,27 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.databinding.DataBindingUtil
+import id.ac.ui.cs.mobileprogramming.michaelsusanto.helloworld.databinding.FragmentWifiBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class WifiFragment : Fragment() {
+
+    private lateinit var binding: FragmentWifiBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_wifi, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
